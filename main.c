6dfe3af7ea6690bdd2cd509ab443b1e3e029b758
liuhanzhe <542169991@qq.com>
2019-04-10 00:12:28 +0800

    add

--- main.c
+++ main.c
@@ -4,51 +4,91 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "header.h"
+
+unsigned short hanlde_ether(const u_char * packet)
+{
+	struct ether_header *ethernet_protocol;
+	unsigned char *p_mac_string = NULL;
+	unsigned short ethernet_type;
+
+	ethernet_protocol = (struct ether_header *)packet;
+	p_mac_string = (unsigned char *)ethernet_protocol->ether_shost;//获取源mac
+	printf("Mac Source Address is %02x:%02x:%02x:%02x:%02x:%02x\n",*(p_mac_string+0),*(p_mac_string+1),*(p_mac_string+2),*(p_mac_string+3),*(p_mac_string+4),*(p_mac_string+5));
+	p_mac_string = (unsigned char *)ethernet_protocol->ether_dhost;//获取目的mac
+	printf("Mac Destination Address is %02x:%02x:%02x:%02x:%02x:%02x\n",*(p_mac_string+0),*(p_mac_string+1),*(p_mac_string+2),*(p_mac_string+3),*(p_mac_string+4),*(p_mac_string+5));
+	ethernet_type = ntohs(ethernet_protocol->ether_type);
+	printf("Ethernet type is :%04x\n",ethernet_type);
+	return ethernet_type;
+}
+
+void handle_ip()
+{
+
+}
+
+void handle_arp()
+{
+
+}
+
+void handle_rarp()
+{
+
+}
 
 void handle_packet(u_char * arg, const struct pcap_pkthdr * pkthdr, const u_char * packet)
 {
-  int * id = (int *)arg;
-  
-  printf("id: %d\n", ++(*id));
-  printf("Packet length: %d\n", pkthdr->len);
-  printf("Number of bytes: %d\n", pkthdr->caplen);
-  printf("Recieved time: %s", ctime((const time_t *)&pkthdr->ts.tv_sec)); 
-  
-  int i;
-  for(i=0; i<pkthdr->len; ++i)
-  {
-    printf(" %02x", packet[i]);
-    if( (i + 1) % 16 == 0 )
-    {
-      printf("\n");
-    }
-  }
-  
-  printf("\n\n");
+	unsigned short ethernet_type = hanlde_ether(packet);
+	if(ethernet_type == 0x0800)
+	{
+		handle_ip();
+	}
+	else if(ethernet_type == 0x0806)
+	{
+		handle_arp();
+	}
+	else if(ethernet_type == 0x0835)
+	{
+		handle_rarp();
+	}
 }
 
-void load_protocol_config()
-{
+//void load_protocol_config()
+//{
     
+//}
+
+pcap_t* pcap_init()
+{
+	char errBuf[PCAP_ERRBUF_SIZE], * p_net_interface_name;
+
+    //load_protocol_config();
+
+    p_net_interface_name = pcap_lookupdev(errBuf);
+
+    if(NULL == p_net_interface_name)
+	{
+		perror("pcap_lookupdev");
+		exit(-1);
+	}
+
+    printf("open device: %s\n", p_net_interface_name);
+
+    pcap_t *pcap_handle = pcap_open_live(p_net_interface_name, 65535, 1, 0, errBuf); 
+
+    return pcap_handle;
 }
 
 int main()
 {
     
-    char errBuf[PCAP_ERRBUF_SIZE], * device;
-
-    load_protocol_config();
-
-    device = pcap_lookupdev(errBuf);
-
-    printf("open device: %s\n", device);
-
-    pcap_t *descr = pcap_open_live(device, 65535, 1, 0, errBuf);  
+    pcap_t *pcap_handle = pcap_init();
 
     int count = 0;
-    pcap_loop(descr, -1, handle_packet, (u_char *)&count); 
+    pcap_loop(pcap_handle, -1, handle_packet, (u_char *)&count); 
    
-    pcap_close(descr);
+    pcap_close(pcap_handle);
  
 	return 0;
 }
